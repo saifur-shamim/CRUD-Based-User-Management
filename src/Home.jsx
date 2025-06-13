4d165c77d17e3d8b0d261ce03b7e568fad7e4531
“saifur-shamim” <saifurshamim150@gmail.com>
2025-06-13 23:18:28 +0600

    Modified the design of add button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,86 +1,94 @@
-
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const [data, setData] = useState([]);
 
-  const [data, setData] =useState([]);
-
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    let parsedData = JSON.parse(localStorage.getItem('data')) || [];
+    let parsedData = JSON.parse(localStorage.getItem("data")) || [];
     if (!Array.isArray(parsedData)) {
-      parsedData = []; 
+      parsedData = [];
     }
     setData(parsedData);
   }, []);
-  
-  const handleDelete=(id)=> {
+
+  const handleDelete = (id) => {
     const confirm = window.confirm("Would you like to delete?");
-    if(confirm)
-    {
-      let parsedData = JSON.parse(localStorage.getItem('data')) || [];
+    if (confirm) {
+      let parsedData = JSON.parse(localStorage.getItem("data")) || [];
       if (!Array.isArray(parsedData)) {
-        parsedData = []; 
+        parsedData = [];
       }
 
-      let newData = parsedData.filter(data=> data.id!==id); 
+      let newData = parsedData.filter((data) => data.id !== id);
       setData(newData);
-      localStorage.setItem('data', JSON.stringify(newData));
-
+      localStorage.setItem("data", JSON.stringify(newData));
     }
-  }
-  
+  };
 
- // console.log(data);
+  // console.log(data);
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100 w-100'>
+    <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100 w-100">
       <h1>List of users </h1>
       <div className="w-90 rounded bg-white border shadow p-4">
-      <div className='d-flex justify-content-end'>
-      <Link to="/create" className='btn btn-success'> Add+ </Link>
-      </div>
+        <div className="d-flex justify-content-center">
+          <Link to="/create" className="btn btn-success">
+            Add
+          </Link>
+        </div>
 
-      <table className='table table-striped'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-       
-        <tbody>
-        {
-           data.map((d,i)=>(
-              <tr key={i}> 
-
-                <td>{d.id}</td>
-                <td>{d.name}</td>
-                <td>{d.email}</td>
-                <td>{d.phone}</td>
-                <td>
-                <Link to={`/read/${d.id}`}className='btn btn-sm btn-info me-2'>Read</Link>
-                <Link to={`/update/${d.id}`}className='btn btn-sm btn-primary me-2'>Edit </Link>
-                <button onClick={e=> handleDelete(d.id)}className='btn btn-sm btn-danger'>Delete </button>
-                </td>
-                
-                
+        {data.length > 0 ? (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
               </tr>
-            ))
-          }
-          
-        </tbody>
-      </table>
+            </thead>
+
+            <tbody>
+              {data.map((d, i) => (
+                <tr key={i}>
+                  <td>{d.id}</td>
+                  <td>{d.name}</td>
+                  <td>{d.email}</td>
+                  <td>{d.phone}</td>
+                  <td>
+                    <Link
+                      to={`/read/${d.id}`}
+                      className="btn btn-sm btn-info me-2"
+                    >
+                      Read
+                    </Link>
+                    <Link
+                      to={`/update/${d.id}`}
+                      className="btn btn-sm btn-primary me-2"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={() => handleDelete(d.id)}
+                      className="btn btn-sm btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="text-center mt-4"> No user records available. </p>
+        )}
       </div>
     </div>
-  )
-
-
+  );
 }
 
-export default Home
+export default Home;
